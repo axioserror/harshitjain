1370eb1ab58ec6390799b70732e0473e38ff0086
axioserror <axioserrorandsyntaxerror@gmail.com>
2024-10-13 00:06:09 +0530

    Update Cards.jsx

--- src/pages/Jobs/Cards.jsx
+++ src/pages/Jobs/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Cards = ({ title, company,location,date, description }) => {
   return (
     <div className='w-fit p-9 border-2 md:mt-4 mt-16 mx-auto rounded-xl border-white bg-black text-white'>
-      <div className='border-b-2 py-2 flex flex-col md:flex-row justify-between gap-20 items-center'>
+      <div className='border-b-2 py-2 flex flex-col md:flex-row md:justify-between gap-20 items-center'>
         <div>
           <h1 className='font-poppins text-xl font-medium'>{title}</h1>
           <p className='text-black my-2 bg-white w-fit px-2 rounded-sm text-base font-semibold font-inconsolata-wide text-nowrap'>{company}</p>
