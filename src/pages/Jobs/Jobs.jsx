34257ec467807db56ac04e9717a43ad4ffb72e1d
axioserror <axioserrorandsyntaxerror@gmail.com>
2024-10-13 00:13:45 +0530

    Update Jobs.jsx

--- src/pages/Jobs/Jobs.jsx
+++ src/pages/Jobs/Jobs.jsx
@@ -16,9 +16,9 @@ const Jobs = () => {
     data-aos="fade-up">
        <p className=" ml-8 text-xs bg-white/65 p-2 rounded-xl w-fit text-white">⚪ Wrok Experience</p>
        <div className='flex lg:flex-row flex-col justify-around  mt-10 px-6 '>
-        <div className='md:w-1/2 '>
+        <div className='lg:w-1/2 '>
         <h1 className='font-poppins font-normal text-4xl p-4'>Lorem ipsum dolor sit<br/> amet.</h1>
-        <p className='font-inconsolata-condensed md:w-2/3 mt-8 px-4 text-justify '>
+        <p className='font-inconsolata-condensed lg:w-2/3 mt-8 px-4 text-justify '>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis possimus ex veritatis asperiores, laudantium eos at omnis explicabo magni culpa labore, officiis recusandae dignissimos. Fugit saepe veritatis dolore magni corporis odit assumenda ipsam dolorum, quam nostrum qui. Nulla culpa eligendi tenetur, officiis facilis esse perspiciatis rerum, repellat quidem sint at.</p>
         </div>
         <div className='md:w-1/2 '>  
